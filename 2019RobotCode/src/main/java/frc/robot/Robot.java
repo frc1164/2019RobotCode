5b84b09c02854fcdc4b82698f3bf8dc485d0ff1f
Elijah <elijahjvela@gmail.com>
2019-01-31 19:48:43 -0700

    Creat empty command based code

--- 2019RobotCode/src/main/java/frc/robot/Robot.java
+++ 2019RobotCode/src/main/java/frc/robot/Robot.java
@@ -7,16 +7,13 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-//import edu.wpi.first.wpilibj.
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,22 +23,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  public static OI m_oi;
 
-  TalonSRX Talon1 = new TalonSRX(1); 
-  TalonSRX Talon2 = new TalonSRX(2);
-  TalonSRX Talon3 = new TalonSRX(3);
-  Spark Spark1 = new Spark(1);
-  Spark Spark2 = new Spark(2);
-  Spark Spark3 = new Spark(3);
-  Spark Spark4 = new Spark(4);
-
-  Joystick Stick = new Joystick(0);
-
-
+  Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
    * This function is run when the robot is first started up and should be
@@ -49,9 +35,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    m_oi = new OI();
+    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    // chooser.addOption("My Auto", new MyAutoCommand());
+    SmartDashboard.putData("Auto mode", m_chooser);
   }
 
   /**
@@ -67,21 +54,45 @@ public class Robot extends TimedRobot {
   }
 
   /**
+   * This function is called once each time the robot enters Disabled mode.
+   * You can use it to reset any subsystem information you want to clear when
+   * the robot is disabled.
+   */
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  /**
    * This autonomous (along with the chooser code above) shows how to select
    * between different autonomous modes using the dashboard. The sendable
    * chooser code works with the Java SmartDashboard. If you prefer the
    * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * getString code to get the auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>You can add additional auto modes by adding additional commands to the
+   * chooser code above (like the commented example) or additional comparisons
+   * to the switch structure below with additional strings & commands.
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    m_autonomousCommand = m_chooser.getSelected();
+
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.start();
+    }
   }
 
   /**
@@ -89,14 +100,17 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
   }
 
@@ -105,37 +119,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-
-    //arcade drive 
-
-    //left joystick
-    Spark1.set(Stick.getRawAxis(1));
-    Spark2.set(Stick.getRawAxis(1));
-    Spark3.set(-Stick.getRawAxis(1));
-    Spark4.set(-Stick.getRawAxis(1));
-    Talon3.set(ControlMode.PercentOutput, -Stick.getRawAxis(0));
-    
-      /*
-      // set left drive to left joystick value
-      Spark1.set(Stick.getRawAxis(1));
-      Spark2.set(Stick.getRawAxis(1));
-   
-      //set right drive to right joystick
-      Spark3.set(-Stick.getRawAxis(3));
-      Spark4.set(-Stick.getRawAxis(3));
-    */
-    if (Stick.getRawButton(4) == true) {// move climbing arm up on button press and stop on release
-      Talon1.set(ControlMode.PercentOutput, 0.1);
-      Talon2.set(ControlMode.PercentOutput, -0.1);
-    } else if (Stick.getRawButton(2) == true) {
-      Talon1.set(ControlMode.PercentOutput, -0.1);
-      Talon2.set(ControlMode.PercentOutput, 0.1);
-    } else {
-      Talon1.set(ControlMode.PercentOutput, 0); 
-      Talon2.set(ControlMode.PercentOutput, 0);
-    }// end ifelse
-
-  }// end teleopPeriodic
+    Scheduler.getInstance().run();
+  }
 
   /**
    * This function is called periodically during test mode.
@@ -143,5 +128,4 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
   }
-
 }
