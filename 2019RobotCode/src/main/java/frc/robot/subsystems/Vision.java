b81351df015433513406f77871606f6088126ae2
ElijahVela <evela@nmsu.edu>
2019-02-09 11:47:18 -0700

    Frame/DataRate stuff added
    Stuff to use when setting the frame rate of the camera

--- 2019RobotCode/src/main/java/frc/robot/subsystems/Vision.java
+++ 2019RobotCode/src/main/java/frc/robot/subsystems/Vision.java
@@ -8,6 +8,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.cscore.UsbCamera;
+
 
 /**
  * Add your docs here.
@@ -15,10 +18,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Vision extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+  private UsbCamera pixyTest = CameraServer.getInstance().startAutomaticCapture();
+  private int cameraFrameRate = 0;
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void setCameraFrameRate(int frameRate){
+    this.cameraFrameRate = frameRate;
+    pixyTest.setFPS(frameRate);
+
+  }
+
+  public double getCameraFrameRate(){
+    return pixyTest.getActualFPS();
+  }
+
+  public double getCameraDataRate(){
+
+    return pixyTest.getActualDataRate();
   }
 }
