6ae06c9fc19a6bebf4796a01d001b680db9e65a1
Elijah <elijahjvela@gmail.com>
2019-02-26 19:51:24 -0700

    Change Axis

--- 2019RobotCode/src/main/java/frc/robot/commands/RunBallGrabberWithJoystick.java
+++ 2019RobotCode/src/main/java/frc/robot/commands/RunBallGrabberWithJoystick.java
@@ -30,7 +30,7 @@ public class RunBallGrabberWithJoystick extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.robotEndEffector.GrabBall(OI.deadband(0.05, stick.getRawAxis(RobotMap.DriverLeftX)));
+    Robot.robotEndEffector.GrabBall(OI.deadband(0.05, stick.getRawAxis(RobotMap.DriverLeftY)));
   }
 
   // Make this return true when this Command no longer needs to run execute()
