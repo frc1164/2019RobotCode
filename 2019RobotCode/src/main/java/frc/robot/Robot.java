02293ccf239f608b896258b288607761eba8d1d3
Elijah <elijahjvela@gmail.com>
2019-02-28 18:21:55 -0700

    Revert "Merge branch 'Lift' into master"
    This reverts commit 00315dcb13c565e23b2d21baef9058ac69f6002f, reversing
    changes made to 91e10cc72fef2dee58211bde741c29cdbd185915.

--- 2019RobotCode/src/main/java/frc/robot/Robot.java
+++ 2019RobotCode/src/main/java/frc/robot/Robot.java
@@ -10,11 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.logic.DriveController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Chassis;
 import frc.robot.subsystems.EndEffector;
-import frc.robot.subsystems.Lift;
 import frc.robot.subsystems.LineSensor;
 
 /**
@@ -28,7 +25,6 @@ public class Robot extends TimedRobot {
   public static Chassis robotChassis = new Chassis();
   public static LineSensor robotLineSensor = new LineSensor();
   public static EndEffector robotEndEffector = new EndEffector();
-  public static Lift robotLift = new Lift();
   public static OI m_OI = new OI();
   public static DriveController robotDriveController = new DriveController();
   
@@ -50,11 +46,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-
-    SmartDashboard.putData(robotChassis);
-    SmartDashboard.putData(robotLineSensor);
-    SmartDashboard.putData(robotEndEffector);
-    SmartDashboard.putData(robotLift);
 
   }
 
